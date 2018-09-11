bb9af26fe9bee6633d1ae610c5746befad932dd8
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2018-09-11 11:25:08 -0500

    Man file added

---
+++ PacPersonGame.java
@@ -0,0 +1,8 @@
+//some includes will likely be here someday
+//some package items will be here someday
+
+public class PacPersonGame{
+    Public static void main(String[] args){
+        //some code will be here someday
+    }
+}
